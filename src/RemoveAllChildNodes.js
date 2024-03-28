e90a60341645c886cf9db9d10b3987e725961853
or <romirov@gmail.com>
2024-03-29 01:17:39 +0300

    create three column three page website

--- src/RemoveAllChildNodes.js
+++
@@ -1,9 +0,0 @@
-/**
- * Функция удаляет все дочерние элементы родителя
- * @param {dom element} parent
- */
-export function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
